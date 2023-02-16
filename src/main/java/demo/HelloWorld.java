bf9e313fbaa3cbe4c798b18f40359a7a4e4a29db
Pankaj <pankajbharambe101@gmail.com>
2023-02-16 17:04:12 +0530

    changing print statement

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -3,7 +3,8 @@ package demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+
+		System.out.println("Hello world through cmd maven");
 
 	}
 
