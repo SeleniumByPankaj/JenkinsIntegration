f23dc51402e08f5c43e4398bc4c44fadcd879209
Pankaj <pankajbharambe101@gmail.com>
2023-02-22 23:08:23 +0530

    adding files

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello world through cmd maven");
+		System.out.println("Hello world through cmd maven. My name is Pankaj");
 
 	}
 
